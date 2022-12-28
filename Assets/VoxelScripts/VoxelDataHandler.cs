5a7720af80c391e8cac47b6212d7aa0edb8668ad
VojtaSara <vojta.sara@gmail.com>
2022-12-28 13:35:45 +0100

    First voxel logic
    First version of voxel logic using mesh renderer - doesn't respond to user input properly at the moment.

---
+++ Assets/VoxelScripts/VoxelDataHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VoxelDataHandler
+{
+    public VoxelData data;
+    float GlobalSceneScale;
+    private InputData _inputData;
+    private Vector3 controllerPosition;
+
+    public VoxelDataHandler(float scale, InputData inputData)
+    {
+        this.data = new VoxelData();
+        data.ChangeData(2, 2, 2, 1);
+        data.ChangeData(2, 2, 4, 1);
+        this.GlobalSceneScale = scale;
+        this._inputData = inputData;
+    }
+
+    public void Update()
+    {
+        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 controllerPosition))
+        {
+            if (_inputData._rightController.TryGetFeatureValue(CommonUsages.trigger, out float triggerPos))
+            {
+                if (triggerPos > 0.5f)
+                {
+                    Vector3Int voxelPos = realCoordsToGridCoords(controllerPosition);
+                    if (data.GetCell(voxelPos.x, voxelPos.y, voxelPos.z) == 0)
+                    {
+                        data.ChangeData(voxelPos.x, voxelPos.y, voxelPos.z, 1);
+                    }
+                    else
+                    {
+                        data.ChangeData(voxelPos.x, voxelPos.y, voxelPos.z, 0);
+                    }
+                }
+            }
+            System.Random r = new System.Random();
+/*            Vector3 GridCoords = realCoordsToGridCoords(rightHandCoords());
+
+            if (data.GetCell((int)GridCoords.x, (int)GridCoords.y, (int)GridCoords.z) == 0)
+            {
+                data.ChangeData((int)GridCoords.x, (int)GridCoords.y, (int)GridCoords.z, 1);
+            }
+            else
+            {
+                data.ChangeData((int)GridCoords.x, (int)GridCoords.y, (int)GridCoords.z, 0);
+            }*/
+        }
+    }
+
+    private Vector3 rightHandCoords()
+    {
+        throw new NotImplementedException();
+    }
+
+    private Vector3Int realCoordsToGridCoords(Vector3 handPosition)
+    {
+        Vector3Int GridPosition = new Vector3Int((int)(handPosition.x / GlobalSceneScale), (int)(handPosition.y / GlobalSceneScale), (int)(handPosition.z / GlobalSceneScale));
+        if (GridPosition.x < 0 || GridPosition.x >= data.Width || GridPosition.y < 0 || GridPosition.y >= data.Depth || GridPosition.z < 0 || GridPosition.z >= data.Height)
+        {
+            return new Vector3Int(0, 0, 0);
+        }
+        else
+        {
+            return GridPosition;
+        }
+    }
+}
---
+++ Assets/VoxelScripts/VoxelRender.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer), typeof(InputData))]
+public class VoxelRender : MonoBehaviour
+{
+    Mesh mesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+    VoxelDataHandler voxelDataHandler;
+
+    private InputData _inputData;
+
+    public float scale = 1f;
+
+    float adjScale;
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        adjScale = scale * 0.5f;
+    }
+
+    void Start()
+    {
+        _inputData = GetComponent<InputData>();
+
+        voxelDataHandler = new VoxelDataHandler(adjScale, _inputData);
+        GenerateVoxelMesh(voxelDataHandler.data);
+        UpdateMesh();
+    }
+
+    void Update()
+    {
+        voxelDataHandler.Update();
+        GenerateVoxelMesh(voxelDataHandler.data);
+        UpdateMesh();
+    }
+
+    private void GenerateVoxelMesh(VoxelData data)
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        for (int z = 0; z < data.Height; z++)
+        {
+            for (int y = 0; y < data.Depth; y++)
+            {
+                for (int x = 0; x < data.Width; x++)
+                {
+                    if (data.GetCell(x, y, z) == 0)
+                    {
+                        continue;
+                    }
+                    MakeCube(adjScale, new Vector3((float)x * scale, (float)y * scale, (float)z * scale), x, y, z, data);
+                }
+            }
+        }
+    }
+    void MakeCube(float cubeScale, Vector3 cubePos, int x, int y, int z, VoxelData data)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (data.GetNeighbour(x,y,z,(Direction)i) == 0)
+            {
+                MakeFace((Direction)i, cubeScale, cubePos);
+            }
+        }
+    }
+
+    void MakeFace(Direction dir, float faceScale, Vector3 facePos)
+    {
+        vertices.AddRange(CubeMeshData.faceVertices(dir, faceScale, facePos));
+        int vCount = vertices.Count;
+
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 1);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4 + 3);
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+
+}
